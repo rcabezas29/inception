304812d02c2d939977cba1fa71772a537eea8393
Raúl Cabezas González <raul.29cg@gmail.com>
2021-06-04 13:06:58 +0200

    Funciona coneccion entre base de datos y wp

--- srcs/requirements/mariadb/srcs/setup.sh
+++ srcs/requirements/mariadb/srcs/setup.sh
@@ -2,6 +2,6 @@ rc-service mariadb restart
 sleep 3
 
 mysql -e "CREATE DATABASE wp_db;"
-mysql -e "CREATE USER 'rcabezas'@'%' IDENTIFIED BY 'rcabezas';"
+mysql -e "CREATE USER 'rcabezas'@'%' IDENTIFIED BY 'password';"
 mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'rcabezas'@'%';"
 mysql -e "FLUSH PRIVILEGES;"
