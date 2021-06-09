ffd8971510ae434f5e3f1ef14924e71f4d45753c
Raúl Cabezas González <raul.29cg@gmail.com>
2021-06-09 12:08:12 +0200

    Arreglando mariadb

--- srcs/requirements/mariadb/srcs/setup.sh
+++ srcs/requirements/mariadb/srcs/setup.sh
@@ -1,5 +1,4 @@
-rc-service mariadb restart
-sleep 3
+/usr/bin/mysqld --user=mysql --datadir=/var/lib/mysql --plugin-dir=/usr/lib/mariadb/plugin --pid-file=/run/mysqld/mariadb.pid --port=3306 --log-error=/var/log/mysqld.log --log-warnings=4 --skip-networking=0
 
 mysql -e "CREATE DATABASE wp_db;"
 mysql -e "CREATE USER 'rcabezas'@'%' IDENTIFIED BY 'password';"
