d8e16791cce821b6b394f02a2bf94a3c1353fea4
Raúl Cabezas González <raul.29cg@gmail.com>
2021-06-14 17:52:21 +0200

    Rehaciendo con fastcgi_pass

--- srcs/requirements/mariadb/srcs/setup.sh
+++ srcs/requirements/mariadb/srcs/setup.sh
@@ -1,8 +1,6 @@
 rc-service mariadb restart
 sleep 3
 
-mysql -e "SET GLOBAL general_log=1;"
-mysql -e "SET GLOBAL general_log_file='/var/log/mysql/mariadb.log';"
 mysql -e "CREATE DATABASE wp_db;"
 mysql -e "CREATE USER 'rcabezas'@'%' IDENTIFIED BY 'password';"
 mysql -e "GRANT ALL PRIVILEGES ON wp_db.* TO 'rcabezas'@'%';"
