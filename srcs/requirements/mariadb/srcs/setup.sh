76f7bc5b65b6c0f1b5ab0564c6bc8e5e75c31e40
Raúl Cabezas González <raul.29cg@gmail.com>
2021-06-18 15:47:40 +0200

    Fallo con mariadb al meterlo en la VM

--- srcs/requirements/mariadb/srcs/setup.sh
+++ srcs/requirements/mariadb/srcs/setup.sh
@@ -3,5 +3,5 @@ sleep 3
 
 mysql -e "CREATE DATABASE wp_db;"
 mysql -e "CREATE USER 'rcabezas'@'%' IDENTIFIED BY 'password';"
-mysql -e "GRANT ALL PRIVILEGES ON wp_db.* TO 'rcabezas'@'%';"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'rcabezas'@'%';"
 mysql -e "FLUSH PRIVILEGES;"
