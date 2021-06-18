61d80fa35c14db4edb65709a0e419e95d0c7c442
Raúl Cabezas González <raul.29cg@gmail.com>
2021-06-18 20:38:36 +0200

    Error en el volumen db

--- srcs/requirements/mariadb/srcs/setup.sh
+++ srcs/requirements/mariadb/srcs/setup.sh
@@ -1,7 +1,7 @@
 rc-service mariadb restart
 sleep 3
 
-mysql -e "CREATE DATABASE wp_db;"
-mysql -e "CREATE USER 'rcabezas'@'%' IDENTIFIED BY 'password';"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'rcabezas'@'%';"
+mysql -e "CREATE DATABASE $DB_NAME;"
+mysql -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASSWD';"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%';"
 mysql -e "FLUSH PRIVILEGES;"
--- srcs/requirements/wordpress/srcs/wp-config.php
+++ srcs/requirements/wordpress/srcs/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wp_db' );
+define( 'DB_NAME', getenv('DB_NAME') );
 
 /** MySQL database username */
-define( 'DB_USER', 'rcabezas' );
+define( 'DB_USER', getenv('DB_USER') );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'password' );
+define( 'DB_PASSWORD', getenv('DB_USER_PASSWD') );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'mariadb' );
